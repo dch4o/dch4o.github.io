95a782c6e8161107697f2c8ad78b41142d346b37
dch4o <dohooncho@gmail.com>
2025-03-28 10:15:51 +0000

    Change default site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://dch4o.github.io",
   integrations: [preact()]
 });
