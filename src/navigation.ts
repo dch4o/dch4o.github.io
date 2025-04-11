9ce214a654343640a7ce3146e75b5cf03e8832ac
dch4o <dohooncho@gmail.com>
2025-04-11 20:32:51 +0000

    Update navigation bar

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,37 +1,49 @@
-import { link } from 'node:fs';
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { link } from "node:fs";
+import { getPermalink, getBlogPermalink, getAsset } from "./utils/permalinks";
 
 export const headerData = {
   links: [
     {
-      text: 'About',
-      href: '/',
+      text: "About",
+      href: "/#about",
     },
     {
-      text: 'CV',
-      href: getPermalink('/personal/cv'),
+      text: "CV",
+      href: getPermalink("/personal/cv"),
     },
     {
-      text: 'Blog',
+      text: "Blog",
       links: [
-        { text: 'All Posts', href: getBlogPermalink() },
-        { text: 'Paper Reviews', href: getPermalink('papers', 'category') },
-        { text: 'Development', href: getPermalink('development', 'category') },
-        { text: 'Life (Korean)', href: getPermalink('life', 'category') },
-      ]
+        { text: "All Posts", href: getBlogPermalink() },
+        { text: "Paper Reviews", href: getPermalink("papers", "category") },
+        { text: "Development", href: getPermalink("development", "category") },
+        { text: "Life (Korean)", href: getPermalink("life", "category") },
+      ],
     },
-    { 
-      text: 'Github', 
-      href: 'https://github.com/dch4o/dch4o.github.io',
+    {
+      icon: "tabler:brand-github",
+      text: "Source",
+      href: "https://github.com/dch4o/dch4o.github.io",
     },
   ],
 };
 
 export const footerData = {
   socialLinks: [
-    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/dch4o' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/dch4o' },
+    {
+      ariaLabel: "LinkedIn",
+      icon: "tabler:brand-linkedin",
+      href: "https://www.linkedin.com/in/dch4o",
+    },
+    { ariaLabel: "RSS", icon: "tabler:rss", href: getAsset("/rss.xml") },
+    {
+      ariaLabel: "Github",
+      icon: "tabler:brand-github",
+      href: "https://github.com/dch4o",
+    },
   ],
-  footNote: `Copyright © 2025 | Dohoon Cho`,
+  footNote: `
+  <div>Copyright © 2025 | Dohoon Cho</div>
+  <div>Last updated on April 2025. This blog is built based on <a class="text-blue-500 underline dark:text-muted" href="https://github.com/onwidget/astrowind"> Astrowind</a>.</div>
+  `,
 };
